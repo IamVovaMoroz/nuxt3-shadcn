182a16222e58736fbe2d43b42f8a4984bd7aa328
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-08-13 18:47:14 +0200

    ..

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ const TabsToday = resolveComponent('TabsToday')
 const list = [
 	{
 		"title": "Today",
-		"component": resolveComponent("TabsToday")
+		"component": TabsToday
 	},
 	{
 		"title": "Week",
@@ -32,16 +32,28 @@ let data = ref([
 ])
 
 let categories = ref({
-	'today': [],
-	'week': [],
-	'month': [],
-	'year': []
+	today: [
+		"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
+		"07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
+		"14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
+		"21:00", "22:00", "23:00"
+	],
+	week: [
+		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+	],
+	month: [
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+		"Oct", "Nov", "Dec"
+	],
+	year: [
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+		"Oct", "Nov", "Dec"
+	]
+});
 
-})
-
-let currentCategory = ref([
-	'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-	'Oct', 'Nov', 'Dec'
+let currentCategory = ref(['today'
+	// 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+	// 'Oct', 'Nov', 'Dec'
 ])
 
 const options = {
@@ -57,9 +69,9 @@ const options = {
 	},
 
 	xAxis: {
-		gridLineColor: 'transparent',
-		categories: currentCategory
-	},
+    gridLineColor: 'transparent',
+    categories: categories.value[currentCategory.value]
+  },
 	yAxis: {
 		gridLineColor: 'transparent',
 		title: {
@@ -95,6 +107,28 @@ const options = {
 	}]
 }
 
+const setCategory = (e) => {
+  const v = e.target.innerText.toLowerCase()
+  currentCategory.value = v
+  switch (v) {
+    case 'today':
+      data.value = generateRandomData(24)
+      break
+    case 'week':
+      data.value = generateRandomData(7)
+      break
+    case 'month':
+      data.value = generateRandomData(31)
+      break
+    case 'year':
+      data.value = generateRandomData(365)
+      break
+    default:
+      data.value = generateRandomData(24)
+      break
+  }
+}
+
 function generateRandomData(numPoints = 12, min = -10, max = 40) {
 	const newData = [];
 	for (let i = 0; i < numPoints; i++) {
@@ -121,7 +155,7 @@ onMounted(() => {
 		</header>
 		<main class="grid gap-4">
 			<Tabs default-value="Today">
-				<TabsList class="max-w-[400px]">
+				<TabsList class="max-w-[400px]" @click="setCategory">
 					<TabsTrigger v-for="item, index in list" :key="index" :value="item.title">
 						{{ item.title }}
 					</TabsTrigger>
